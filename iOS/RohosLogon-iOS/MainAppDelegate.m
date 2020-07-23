79ad3bedb7704960c4a4447898940992b34e9299
Dmytro Bogovych <dmytro.bogovych@gmail.com>
2020-07-23 11:01:29 +0300

    - fix UI

--- iOS/RohosLogon-iOS/MainAppDelegate.m
+++ iOS/RohosLogon-iOS/MainAppDelegate.m
@@ -53,4 +53,10 @@
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
+-(UIInterfaceOrientationMask)application:(UIApplication *)application supportedInterfaceOrientationsForWindow:(UIWindow *)window
+{
+    // Restrict to portrait mode
+    return UIInterfaceOrientationMaskPortrait;
+}
+
 @end
